a7b8462cda8f76cf152e72e4446c46d46e8d6da9
Rob Cameron <rwcameron@gmail.com>
2015-06-29 04:23:47 +0000

    inital

---
+++ pyunicorn-ui.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import unicornhat
+import time
+
+class UnicornHatManager:
+    def __init__(self, brightness):
+        unicornhat.brightness = brightness
+    def set_rotation(self,rotation):
+        unicornhat.rotation(rotation)
+    def set_pixel(self, x,y,r,b,g):
+        unicornhat.set_pixel(x,y,r,b,g)
+        unicornhat.show()
+
+um = UnicornHatManager(0.1)
+um.set_rotation(0)
+
+heart = [[0, 1, 1, 0, 0, 1, 1, 0],
+         [1, 1, 1, 1, 1, 1, 1, 1],
+         [1, 1, 1, 1, 1, 1, 1, 1],
+         [1, 1, 1, 1, 1, 1, 1, 1],
+         [0, 1, 1, 1, 1, 1, 1, 0],
+         [0, 1, 1, 1, 1, 1, 1, 0],
+         [0, 0, 1, 1, 1, 1, 0, 0],
+         [0, 0, 0, 1, 1, 0, 0, 0]]
+
+def draw_heart():
+    for x in range(0,8):
+     for y in range(0,8):
+         if heart[x][y]:
+             if x == 0:
+                 r, g, b = 255, 0, 0
+             elif  x == 1:
+                 r, g, b = 240, 128,0
+             elif  x == 2:
+                 r, g, b = 255, 255, 0
+             elif  x == 3:
+                 r, g, b = 0, 255, 0
+             elif  x == 4:
+                 r, g, b = 0, 0, 255
+             elif  x == 5:
+                 r, g, b = 127, 0, 255
+             elif  x == 6:
+                 r, g, b = 255, 0, 255
+             elif  x == 7:
+                 r, g, b = 255, 0, 127
+
+             um.set_pixel(x,y,r,g,b)
+
+draw_heart()
+time.sleep(1)
+unicornhat.clear()
+um.set_rotation(90)
+draw_heart()
+time.sleep(1)
+unicornhat.clear()
+um.set_rotation(180)
+draw_heart()
+time.sleep(1)
+unicornhat.clear()
+um.set_rotation(270)
+draw_heart()
+time.sleep(1)
+unicornhat.clear()
+um.set_rotation(0)
+draw_heart()
+time.sleep(1)
+unicornhat.clear()
+
+time.sleep(5)
---
+++ simple.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import unicornhat as UH
+import time
+
+UH.set_pixel(0,0,255,0,0)
+UH.set_pixel(7,0,255,0,0)
+UH.set_pixel(1,1,255,0,0)
+UH.set_pixel(6,1,255,0,0)
+UH.set_pixel(2,2,255,0,0)
+UH.set_pixel(5,2,255,0,0)
+UH.set_pixel(3,3,255,0,0)
+UH.set_pixel(4,3,255,0,0)
+UH.set_pixel(4,4,255,0,0)
+UH.set_pixel(3,2,255,0,0)
+UH.set_pixel(5,5,255,0,0)
+UH.set_pixel(6,6,255,0,0)
+UH.set_pixel(7,7,255,0,0)
+UH.show()
+
+time.sleep(15)
