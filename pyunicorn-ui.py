f2bf96ed242b8ed737a2782e7aca15c3ad79e4bd
Rob Cameron <rwcameron@gmail.com>
2015-07-10 06:13:44 +0000

    add new tests

--- pyunicorn-ui.py
+++ pyunicorn-ui.py
@@ -2,10 +2,11 @@
 
 import unicornhat
 import time
+import random
 
 class UnicornHatManager:
     def __init__(self, brightness):
-        unicornhat.brightness = brightness
+        unicornhat.brightness(brightness)
     def set_rotation(self,rotation):
         unicornhat.rotation(rotation)
     def set_pixel(self, x,y,r,b,g):
@@ -23,6 +24,18 @@ heart = [[0, 1, 1, 0, 0, 1, 1, 0],
          [0, 1, 1, 1, 1, 1, 1, 0],
          [0, 0, 1, 1, 1, 1, 0, 0],
          [0, 0, 0, 1, 1, 0, 0, 0]]
+
+def random_color():
+    return random.randrange(0,255)
+
+def random_pattern():
+    for x in range(0,8):
+        for y in range (0,8):
+            r = random_color()
+            g = random_color()
+            b = random_color()
+            um.set_pixel(x,y,r,g,b)
+
 
 def draw_heart():
     for x in range(0,8):
@@ -44,27 +57,32 @@ def draw_heart():
                  r, g, b = 255, 0, 255
              elif  x == 7:
                  r, g, b = 255, 0, 127
-
              um.set_pixel(x,y,r,g,b)
 
-draw_heart()
-time.sleep(1)
-unicornhat.clear()
-um.set_rotation(90)
-draw_heart()
-time.sleep(1)
-unicornhat.clear()
-um.set_rotation(180)
-draw_heart()
-time.sleep(1)
-unicornhat.clear()
-um.set_rotation(270)
-draw_heart()
-time.sleep(1)
-unicornhat.clear()
-um.set_rotation(0)
-draw_heart()
-time.sleep(1)
-unicornhat.clear()
+while True:
+    random_pattern()
+    time.sleep(1)
 
-time.sleep(5)
+#draw_heart()
+#time.sleep(0.1)
+#unicornhat.clear()
+
+'''
+while True:
+    um.set_rotation(90)
+    draw_heart()
+    time.sleep(0.1)
+    unicornhat.clear()
+    um.set_rotation(180)
+    draw_heart()
+    time.sleep(0.1)
+    unicornhat.clear()
+    um.set_rotation(270)
+    draw_heart()
+    time.sleep(0.1)
+    unicornhat.clear()
+    um.set_rotation(0)
+    draw_heart()
+    time.sleep(0.1)
+    unicornhat.clear()
+'''
